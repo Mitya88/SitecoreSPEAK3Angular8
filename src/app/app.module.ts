18e778e3fae8f5f93d92e64728adac8815948fb7
Mihaly Árvai <mihaly.arvai@mirumagency.com>
2020-08-12 12:26:41 +0200

    ini

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScPageModule } from '@speak/ng-bcl/page';
+import { ScContextDetailsModule } from '@speak/ng-bcl/context-details';
+import { ScIconModule } from '@speak/ng-bcl/icon';
+import {ScGlobalHeaderModule} from '@speak/ng-bcl/global-header'
+import {ScGlobalLogoModule} from '@speak/ng-bcl/global-logo'
+import { ScMenu, ScMenuModule } from '@speak/ng-bcl/menu';
+import {ScApplicationHeaderModule} from '@speak/ng-bcl/application-header'
 
 @NgModule({
   declarations: [
@@ -10,7 +17,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ScPageModule,
+    ScContextDetailsModule,
+    ScIconModule,
+    ScGlobalHeaderModule,
+    ScMenuModule,
+    ScGlobalLogoModule,
+    ScApplicationHeaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
